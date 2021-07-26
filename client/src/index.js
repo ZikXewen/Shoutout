4784d48a7daee501afc3b8bb43183f2d4a7bb69a
ZikXewen <datasup12@gmail.com>
2021-07-26 11:57:39 +0700

    Font

--- client/src/index.js
+++ client/src/index.js
@@ -5,18 +5,26 @@ import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import thunk from "redux-thunk";
 import checkTimeout from "./middlewares/checkTimeout";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
 
 import { reducers } from "./reducers";
 import App from "./App";
 import "./index.css";
 
 const store = createStore(reducers, applyMiddleware(checkTimeout, thunk));
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: `"Montserrat"`, // Thai Font?
+  },
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
